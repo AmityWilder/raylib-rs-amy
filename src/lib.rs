f3bfc033e370c82c1d5950fce9a57cccc162e110
Amy Wilder <henrythepony@gmail.com>
2025-06-11 20:07:52 -0400

    Add to module doc comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
 /// Safe-ish wrappers for ffi functions
+///
+/// This is the bare minimum to prevent UB
 pub mod low_level;
 
 /// High-level code for covering edge cases and best practice
+///
+/// Adds ergonomics and a safety net against carelessness
 pub mod safe;
